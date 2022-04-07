caa101948a541269b6304359aab092d90b08236a
unknown <you@example.com>
2022-04-07 12:18:41 -0300

    inicializando o React no site TCC

---
+++ Components/Corpo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Corpo = () => {
+  return (
+    <div className='text-xl'>
+        <h2>Segunda linha</h2>
+    </div>
+  )
+}
+
+export default Corpo
---
+++ Components/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Layout = () => {
+
+  return (
+    <div className='Layout text-center justify-center text-zinc-900 text-4xl font-semibold bg-emerald-400 '>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Layout
